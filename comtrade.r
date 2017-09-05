cec2e041aeeb832cf329110fb98e24650c389335
sgennaria <sgennaria@gmail.com>
2017-09-05 06:40:23 -0400

    1

---
+++ comtrade.r
@@ -0,0 +1,78 @@
+library(rjson)
+string <- "http://comtrade.un.org/data/cache/partnerAreas.json"
+reporters <- fromJSON(file=string)
+reporters <- as.data.frame(t(sapply(reporters$results,rbind)))
+
+get.Comtrade <- function(url="http://comtrade.un.org/api/get?"
+                         ,maxrec=50000
+                         ,type="C"
+                         ,freq="A"
+                         ,px="HS"
+                         ,ps="now"
+                         ,r
+                         ,p
+                         ,rg="all"
+                         ,cc="TOTAL"
+                         ,fmt="json"
+)
+{
+  string<- paste(url
+                 ,"max=",maxrec,"&" #maximum no. of records returned
+                 ,"type=",type,"&" #type of trade (c=commodities)
+                 ,"freq=",freq,"&" #frequency
+                 ,"px=",px,"&" #classification
+                 ,"ps=",ps,"&" #time period
+                 ,"r=",r,"&" #reporting area
+                 ,"p=",p,"&" #partner country
+                 ,"rg=",rg,"&" #trade flow
+                 ,"cc=",cc,"&" #classification code
+                 ,"fmt=",fmt        #Format
+                 ,sep = ""
+  )
+  
+  if(fmt == "csv") {
+    raw.data<- read.csv(string,header=TRUE)
+    return(list(validation=NULL, data=raw.data))
+  } else {
+    if(fmt == "json" ) {
+      raw.data<- fromJSON(file=string)
+      data<- raw.data$dataset
+      validation<- unlist(raw.data$validation, recursive=TRUE)
+      ndata<- NULL
+      if(length(data)> 0) {
+        var.names<- names(data[[1]])
+        data<- as.data.frame(t( sapply(data,rbind)))
+        ndata<- NULL
+        for(i in 1:ncol(data)){
+          data[sapply(data[,i],is.null),i]<- NA
+          ndata<- cbind(ndata, unlist(data[,i]))
+        }
+        ndata<- as.data.frame(ndata)
+        colnames(ndata)<- var.names
+      }
+      return(list(validation=validation,data =ndata))
+    }
+  }
+}
+
+
+s1 <- get.Comtrade()
+
+
+
+
+
+
+
+# library(xlsx) # don't use this
+library('readxl')
+
+#link <- "http://unstats.un.org/unsd/tradekb/Attachment439.aspx?AttachmentType=1" # the site doesn't link directly to the excel file, but this link downloads it.  would be nice to automate this in the script, but for now i've downloaded it manually.
+fpath <- "C:/Users/shawn.gennaria/Documents/GitHub/comtrade/UN Comtrade Commodity Classifications.xlsx"
+
+df <- read_excel(fpath, excel_sheets(fpath)[1])
+str(df)
+
+fpath2 <- "C:/Users/shawn.gennaria/Documents/GitHub/comtrade/UN Comtrade Country List.xls"
+df <- read_excel(fpath2)
+str(df)
